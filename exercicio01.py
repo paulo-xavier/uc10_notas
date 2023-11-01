ada7831ffd84edabcdc143f265cd76f83c3e462b
paulo-xavier <px568661gmail.com>
2023-11-01 15:04:39 -0300

    my notes code

---
+++ exercicio01.py
@@ -0,0 +1,12 @@
+print("GitHub exercise")
+
+name = input("Enter your name")
+
+grade1 = float(input("grade1"))
+grade2 = float(input("grade2"))
+grade3 = float(input("grade3"))
+grade4 = float(input("grade4"))
+
+# average = (grade1 + grade2 + grade3 + grade4) / 4
+
+print(f" The student: {name} got the average: {(grade1 + grade2 + grade3 + grade4) / 4} ")
